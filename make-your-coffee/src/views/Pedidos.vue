d7a387efd8a56ec2586eb8b8426e1ce77884c840
GeissonChagas <geisson.excel@gmail.com>
2023-01-21 23:42:14 -0300

    esqueleto Home/Pedidos

---
+++ make-your-coffee/src/views/Pedidos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <Banner />
+      <div class="main-container">
+        <h1>Monte o seu café:</h1>
+        <CafeForm />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Banner from '../components/Banner'
+  import CafeForm from '../components/CafeForm.vue'
+  export default {
+    name: 'Home',
+    components: {
+      Banner,
+      CafeForm
+    }
+  }
+  </script>
